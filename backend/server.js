1e010c0093e09f8b996e5e2153080dfbb6a22d90
Mandy <mandychegarty@gmail.com>
2023-11-18 10:21:45 +0000

    express server & initial product routes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+// need to use .js for your own es modules
+import products from "./data/products.js";
+
+const port = 8000;
+
+//initialize express
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("API is running...");
+});
+
+app.get("/api/products", (request, response) => {
+  response.json(products);
+});
+
+app.get("/api/products/:id", (request, response) => {
+  const product = products.find((p) => p._id === request.params.id);
+  response.json(product);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
